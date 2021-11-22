63ccc5e2f93bd40c3d44726ddab182b4bf4f53ed
gergelyn <preszthegas@gmail.com>
2021-11-22 12:18:50 +0100

    CRUD implemented

--- src/main/java/com/webclass/carservice/controller/ClientController.java
+++ src/main/java/com/webclass/carservice/controller/ClientController.java
@@ -1,8 +1,51 @@
 package com.webclass.carservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.webclass.carservice.entity.Client;
+import com.webclass.carservice.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
+    @Autowired
+    ClientRepository clientRepository;
 
+    @GetMapping("/clients")
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    @PostMapping("/clients")
+    public Client createClient(@RequestBody Client newClient) {
+        return clientRepository.save(newClient);
+    }
+
+    @GetMapping("/clients/{id}")
+    public Client getClient(@PathVariable Long id) {
+        return clientRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/clients/{id}")
+    public Client updateClient(@PathVariable Long id, @RequestBody Client newClient) {
+        return clientRepository.findById(id)
+                .map(client -> {
+                    client.setName(newClient.getName());
+                    client.setCarType(newClient.getCarType());
+                    client.setLicenseCode(newClient.getLicenseCode());
+                    client.setIssue(newClient.getIssue());
+                    client.setProgress(newClient.getProgress());
+                    return clientRepository.save(client);
+                })
+                .orElseGet(() -> {
+                    newClient.setId(id);
+                    return clientRepository.save(newClient);
+                });
+    }
+
+    @DeleteMapping("/clients/{id}")
+    public void deleteClient(@PathVariable Long id) {
+        clientRepository.deleteById(id);
+    }
 }
