7a340a7e245b5ff792ffa70ebc142bb40e72bee5
gergelyn <preszthegas@gmail.com>
2021-11-24 11:58:25 +0100

    Securing Clients to their Managers

--- src/main/java/com/webclass/carservice/repository/ClientRepository.java
+++ src/main/java/com/webclass/carservice/repository/ClientRepository.java
@@ -2,9 +2,22 @@ package com.webclass.carservice.repository;
 
 import com.webclass.carservice.entity.Client;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('ROLE_MANAGER')")
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
-    List<Client> findByProgress(String progress);
+    @Override
+    @PreAuthorize("#client?.manager == null or #client?.manager?.email == authentication?.name")
+    Client save(@Param("client") Client client);
+
+    @Override
+    @PreAuthorize("@clientRepository.findById(#id)?.manager?.email == authentication?.name")
+    void deleteById(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("#client?.manager?.email == authentication?.name")
+    void delete(@Param("client") Client client);
 }
