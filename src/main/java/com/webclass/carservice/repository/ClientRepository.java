db42985598372c5b4b86f197bac5eb62a39ef63a
gergelyn <preszthegas@gmail.com>
2021-11-24 09:17:30 +0100

    Extends PagingAndSortingRepository

--- src/main/java/com/webclass/carservice/repository/ClientRepository.java
+++ src/main/java/com/webclass/carservice/repository/ClientRepository.java
@@ -1,10 +1,10 @@
 package com.webclass.carservice.repository;
 
 import com.webclass.carservice.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
     List<Client> findByProgress(String progress);
 }
