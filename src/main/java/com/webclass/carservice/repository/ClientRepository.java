e9b89cc25495884465b8bd8fa5f68d47d4469c2b
gergelyn <preszthegas@gmail.com>
2021-12-12 10:45:27 +0100

    Secure save, delete client with manager

--- src/main/java/com/webclass/carservice/repository/ClientRepository.java
+++ src/main/java/com/webclass/carservice/repository/ClientRepository.java
@@ -1,20 +1,18 @@
 package com.webclass.carservice.repository;
 
 import com.webclass.carservice.entity.Client;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.util.List;
-
 @PreAuthorize("hasRole('ROLE_MANAGER')")
-public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
+public interface ClientRepository extends CrudRepository<Client, Long> {
     @Override
     @PreAuthorize("#client?.manager == null or #client?.manager?.email == authentication?.name")
     Client save(@Param("client") Client client);
 
     @Override
-    @PreAuthorize("@clientRepository.findById(#id)?.manager?.email == authentication?.name")
+    @PreAuthorize("@clientRepository.findById(#id).get().manager.email == authentication?.name")
     void deleteById(@Param("id") Long id);
 
     @Override
