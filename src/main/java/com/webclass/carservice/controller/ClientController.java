da88e081ad9ecbb7c00861fcb7f40b121316b2a3
gergelyn <preszthegas@gmail.com>
2021-12-12 10:44:45 +0100

    Connect logged-in and creator manager to client

--- src/main/java/com/webclass/carservice/controller/ClientController.java
+++ src/main/java/com/webclass/carservice/controller/ClientController.java
@@ -1,33 +1,40 @@
 package com.webclass.carservice.controller;
 
 import com.webclass.carservice.entity.Client;
+import com.webclass.carservice.entity.Manager;
 import com.webclass.carservice.repository.ClientRepository;
+import com.webclass.carservice.repository.ManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ClientController {
     @Autowired
     ClientRepository clientRepository;
 
-    @GetMapping("/clients")
-    public List<Client> getAllClients() {
+    @Autowired
+    ManagerRepository managerRepository;
+
+    @GetMapping("/api/clients")
+    public Iterable<Client> getAllClients() {
         return clientRepository.findAll();
     }
 
-    @PostMapping("/clients")
+    @PostMapping("/api/clients")
     public Client createClient(@RequestBody Client newClient) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Manager manager = this.managerRepository.findByEmail(email);
+        newClient.setManager(manager);
         return clientRepository.save(newClient);
     }
 
-    @GetMapping("/clients/{id}")
+    @GetMapping("/api/clients/{id}")
     public Client getClient(@PathVariable Long id) {
         return clientRepository.findById(id).orElse(null);
     }
 
-    @PutMapping("/clients/{id}")
+    @PutMapping("/api/clients/{id}")
     public Client updateClient(@PathVariable Long id, @RequestBody Client newClient) {
         return clientRepository.findById(id)
                 .map(client -> {
@@ -37,6 +44,9 @@ public class ClientController {
                     client.setLicenseCode(newClient.getLicenseCode());
                     client.setIssue(newClient.getIssue());
                     client.setProgress(newClient.getProgress());
+                    String email = SecurityContextHolder.getContext().getAuthentication().getName();
+                    Manager manager = this.managerRepository.findByEmail(email);
+                    client.setManager(manager);
                     return clientRepository.save(client);
                 })
                 .orElseGet(() -> {
@@ -45,7 +55,7 @@ public class ClientController {
                 });
     }
 
-    @DeleteMapping("/clients/{id}")
+    @DeleteMapping("/api/clients/{id}")
     public void deleteClient(@PathVariable Long id) {
         clientRepository.deleteById(id);
     }
